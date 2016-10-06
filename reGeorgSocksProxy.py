ac0ca8ff100b5c2f7b1ab2c6027fa0cdf94bdb5c
Markus Wulftange <markus.wulftange@gmail.com>
2016-10-06 09:02:51 +0200

    Use httpPath instead of whole URL

--- reGeorgSocksProxy.py
+++ reGeorgSocksProxy.py
@@ -224,7 +224,7 @@ class session(Thread):
         cookie = None
         conn = self.httpScheme(host=self.httpHost, port=self.httpPort)
         # response = conn.request("POST", self.httpPath, params, headers)
-        response = conn.urlopen('POST', self.connectString + "?cmd=connect&target=%s&port=%d" % (target, port), headers=headers, body="")
+        response = conn.urlopen('POST', self.httpPath + "?cmd=connect&target=%s&port=%d" % (target, port), headers=headers, body="")
         if response.status == 200:
             status = response.getheader("x-status")
             if status == "OK":
@@ -256,7 +256,7 @@ class session(Thread):
                     break
                 data = ""
                 headers = {"X-CMD": "READ", "Cookie": self.cookie, "Connection": "Keep-Alive"}
-                response = conn.urlopen('POST', self.connectString + "?cmd=read", headers=headers, body="")
+                response = conn.urlopen('POST', self.httpPath + "?cmd=read", headers=headers, body="")
                 data = None
                 if response.status == 200:
                     status = response.getheader("x-status")
@@ -304,7 +304,7 @@ class session(Thread):
                 if not data:
                     break
                 headers = {"X-CMD": "FORWARD", "Cookie": self.cookie, "Content-Type": "application/octet-stream", "Connection": "Keep-Alive"}
-                response = conn.urlopen('POST', self.connectString + "?cmd=forward", headers=headers, body=data)
+                response = conn.urlopen('POST', self.httpPath + "?cmd=forward", headers=headers, body=data)
                 if response.status == 200:
                     status = response.getheader("x-status")
                     if status == "OK":
